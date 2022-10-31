ef4810422409d3a67c4092bc3199c0881ab57c68
Galshamir8 <galshamir8@gmail.com>
2022-10-31 20:57:03 +0200

    Add username validate

--- app/src/main/java/com/example/permissionauthenticator/validator/Rule.java
+++ app/src/main/java/com/example/permissionauthenticator/validator/Rule.java
@@ -1,5 +1,5 @@
 package com.example.permissionauthenticator.validator;
 
 public abstract class Rule {
-    public abstract boolean validate();
+    public abstract boolean validate(Object... params);
 }
--- app/src/main/java/com/example/permissionauthenticator/validator/UsernameValidator.java
+++ app/src/main/java/com/example/permissionauthenticator/validator/UsernameValidator.java
@@ -1,8 +1,16 @@
 package com.example.permissionauthenticator.validator;
 
+import java.util.regex.Pattern;
+
 public class UsernameValidator extends Rule {
+    private Pattern usernameRegex;
+
+    public UsernameValidator() {
+        usernameRegex = Pattern.compile("[/d/w]{8,}");
+    }
+
     @Override
-    public boolean validate() {
-        return false;
+    public boolean validate(Object... params) {
+        return usernameRegex.matcher((String)(params[0])).find();
     }
 }
