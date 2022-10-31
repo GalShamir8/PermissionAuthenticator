fcd3cd34433f942e45b0c19f040975c30f6a9fc5
Galshamir8 <galshamir8@gmail.com>
2022-10-31 21:15:55 +0200

    Add password validate implementation

--- app/src/main/java/com/example/permissionauthenticator/validator/PasswordValidator.java
+++ app/src/main/java/com/example/permissionauthenticator/validator/PasswordValidator.java
@@ -1,8 +1,15 @@
 package com.example.permissionauthenticator.validator;
+import java.util.regex.Pattern;
 
 public class PasswordValidator extends Rule {
+    private Pattern passwordRegex;
+
+    public PasswordValidator() {
+        passwordRegex = Pattern.compile("[/d/w]{8,}");
+    }
+
     @Override
-    public boolean validate() {
-        return false;
+    public boolean validate(Object... params) {
+        return passwordRegex.matcher((String)(params[0])).find();
     }
 }
