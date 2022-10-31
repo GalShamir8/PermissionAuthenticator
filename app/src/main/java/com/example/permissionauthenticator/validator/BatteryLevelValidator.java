619305f6bcb6d98aee898123d3d57eaa211effee
Galshamir8 <galshamir8@gmail.com>
2022-10-31 21:30:08 +0200

    Add battery level validator

--- app/src/main/java/com/example/permissionauthenticator/validator/BatteryLevelValidator.java
+++ app/src/main/java/com/example/permissionauthenticator/validator/BatteryLevelValidator.java
@@ -1,8 +1,28 @@
 package com.example.permissionauthenticator.validator;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.BatteryManager;
+
 public class BatteryLevelValidator extends Rule {
+    private float requiredLevel = 30;
+    private BroadcastReceiver batteryInfoReceiver;
+    private float currentLevel = 0;
+
+    public BatteryLevelValidator() {
+        batteryInfoReceiver = new BroadcastReceiver(){
+            @Override
+            public void onReceive(Context ctx, Intent intent) {
+                int level = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
+                int scale = intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
+                currentLevel = level * 100 / (float)scale;
+            }
+        };
+    }
+
     @Override
-    public boolean validate() {
-        return false;
+    public boolean validate(Object... params) {
+        return currentLevel >= requiredLevel;
     }
 }
