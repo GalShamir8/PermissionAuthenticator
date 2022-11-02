821390dfef80779ffdc95ad8d70b60414929e7ca
Galshamir8 <galshamir8@gmail.com>
2022-11-02 09:41:41 +0200

    Add c'tor and align validate function in flash sensor validator

--- app/src/main/java/com/example/permissionauthenticator/validator/FlashSensorValidator.java
+++ app/src/main/java/com/example/permissionauthenticator/validator/FlashSensorValidator.java
@@ -1,8 +1,11 @@
 package com.example.permissionauthenticator.validator;
 
 public class FlashSensorValidator extends Rule {
+
+    public FlashSensorValidator() { }
+
     @Override
-    public boolean validate() {
+    public boolean validate(Object... params) {
         return false;
     }
 }
