61bbccfc0686c1c647c6fa6dfa4b575c8dcff157
Galshamir8 <galshamir8@gmail.com>
2022-11-12 15:11:28 +0200

    IUpdate ctor of users, password validators

--- app/src/main/java/com/example/permissionauthenticator/validator/PasswordValidator.java
+++ app/src/main/java/com/example/permissionauthenticator/validator/PasswordValidator.java
@@ -4,12 +4,10 @@ import android.util.Log;
 import java.util.regex.Pattern;
 
 public class PasswordValidator extends Rule {
-    private Pattern passwordRegex;
+    private final Pattern passwordRegex = Pattern.compile("[\\d\\w]{8,}");
     private String passwordInput;
 
-    public PasswordValidator() {
-        passwordRegex = Pattern.compile("[\\d\\w]{8,}");
-    }
+    public PasswordValidator() { }
 
     public void setPasswordInput(String passwordInput){
         this.passwordInput = passwordInput;
--- app/src/main/java/com/example/permissionauthenticator/validator/UsernameValidator.java
+++ app/src/main/java/com/example/permissionauthenticator/validator/UsernameValidator.java
@@ -5,12 +5,10 @@ import android.util.Log;
 import java.util.regex.Pattern;
 
 public class UsernameValidator extends Rule {
-    private Pattern usernameRegex;
+    private Pattern usernameRegex = Pattern.compile("[\\w]{3,}");
     private String usernameInput;
 
-    public UsernameValidator() {
-        usernameRegex = Pattern.compile("[\\w]{3,}");
-    }
+    public UsernameValidator() { }
 
     public void setUsernameInput(String usernameInput){
         this.usernameInput = usernameInput;
